f78143e053ee0241f44a91544a40d051ce393235
Colin Wilmans <colin.wilmans@crwsolutions.nl>
2024-12-23 12:48:21 +0100

    Simple first version

---
+++ src/FluentDefaults/IDefaultRule.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace FluentDefaults;
+
+public interface IDefaultRule<T>
+{
+    void Apply(T instance);
+}
+
+public abstract class AbstractDefaulter<T> : IDefaultRule<T>
+{
+    private readonly List<Action<T>> _defaultActions = new();
+
+    public void Apply(T instance)
+    {
+        foreach (var action in _defaultActions)
+        {
+            action(instance);
+        }
+    }
+    
+    protected void DefaultFor<TProperty>(Expression<Func<T, TProperty>> expression, TProperty defaultValue)
+    {
+        if (expression.Body is MemberExpression memberExpression)
+        {
+            if (memberExpression.Member is PropertyInfo propertyInfo)
+            {
+                var getMethod = propertyInfo.GetGetMethod();
+                var setMethod = propertyInfo.GetSetMethod();
+
+                if (getMethod != null && setMethod != null)
+                {
+                    _defaultActions.Add(instance =>
+                    {
+                        var currentValue = (TProperty?)getMethod.Invoke(instance, null);
+                        if (Equals(currentValue, default(TProperty)))
+                        {
+                            setMethod.Invoke(instance, [defaultValue]);
+                        }
+                    });
+                }
+            }
+            else if (memberExpression.Member is FieldInfo fieldInfo)
+            {
+                _defaultActions.Add(instance =>
+                {
+                    var currentValue = (TProperty?)fieldInfo.GetValue(instance);
+                    if (Equals(currentValue, default(TProperty)))
+                    {
+                        fieldInfo.SetValue(instance, defaultValue);
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ tests/FluentDefaults.Tests/DefaultForTests.cs
@@ -0,0 +1,117 @@
+﻿namespace FluentDefaults.Tests;
+
+public class DefaultForTests
+{
+    [Fact]
+    public void Int_ShouldBeSetToDefault()
+    {
+        var customer = new Customer();
+        var defaulter = new CustomerDefaulter();
+
+        defaulter.Apply(customer);
+
+        Assert.Equal(1, customer.Number1);
+    }
+
+    [Fact]
+    public void NullableInt_ShouldBeSetToDefault()
+    {
+        var customer = new Customer();
+        var defaulter = new CustomerDefaulter();
+
+        defaulter.Apply(customer);
+
+        Assert.Equal(2, customer.Number2);
+    }
+
+    [Fact]
+    public void IntWithValue_ShouldNotBeSetToDefault()
+    {
+        var customer = new Customer
+        {
+            Number1 = 3
+        };
+        var defaulter = new CustomerDefaulter();
+
+        defaulter.Apply(customer);
+
+        Assert.Equal(3, customer.Number1);
+    }
+
+    [Fact]
+    public void NullableIntWithValue_ShouldNotBeSetToDefault()
+    {
+        var customer = new Customer
+        {
+            Number2 = 4
+        };
+        var defaulter = new CustomerDefaulter();
+
+        defaulter.Apply(customer);
+
+        Assert.Equal(4, customer.Number2);
+    }
+
+    [Fact]
+    public void NullableIntWith0_ShouldNotBeSetToDefault()
+    {
+        var customer = new Customer
+        {
+            Number2 = 0
+        };
+        var defaulter = new CustomerDefaulter();
+
+        defaulter.Apply(customer);
+
+        Assert.Equal(0, customer.Number2);
+    }
+
+    [Fact]
+    public void FieldInt_ShouldBeSetToDefault()
+    {
+        var customer = new Customer();
+        var defaulter = new CustomerDefaulter();
+
+        defaulter.Apply(customer);
+
+        Assert.Equal(3, customer.Number3);
+    }
+}
+
+public class Customer
+{
+    public int Number1 { get; set; }
+    public int? Number2 { get; set; }
+
+    public int Number3;
+}
+
+public class CustomerDefaulter : AbstractDefaulter<Customer>
+{
+    public CustomerDefaulter()
+    {
+        DefaultFor(x => x.Number1, 1); // Default Number1 to 1
+        DefaultFor(x => x.Number2, 2); // Default Number2 to null if not set
+        DefaultFor(x => x.Number3, 3); // Default Number2 to null if not set
+    }
+}
+
+public class Address
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+}
+
+public class InvoiceLine
+{
+    public string Description { get; set; }
+    public decimal Amount { get; set; }
+}
+
+public class Invoice
+{
+    public Address Address { get; set; }
+    public List<InvoiceLine> Lines { get; set; }
+    public decimal Total;
+}
+
